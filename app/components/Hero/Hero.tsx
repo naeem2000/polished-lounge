1aba04e8569bdfc582a2c11790c33a282c1be107
Naeem Carr <mogamatnaeemcarr@gmail.com>
2024-05-11 18:30:18 +0200

    first push to dev

---
+++ app/components/Hero/Hero.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Hero.scss';
+
+export default function Hero() {
+	return (
+		<header className='hero'>
+			<h1>awe</h1>
+		</header>
+	);
+}
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Hero from './components/Hero/Hero';
+import Nav from './components/Nav/Nav';
+
+export default function Home() {
+	return (
+		<>
+			<Nav />
+			<Hero />
+		</>
+	);
+}
